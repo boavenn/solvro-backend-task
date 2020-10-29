3a1158dcbda2aae862cddc9765a6be22e0b1a896
Witold Brzozowski <brzozowskyy@gmail.com>
2020-10-29 18:19:30 +0100

    init

---
+++ src/main/java/io/github/boavenn/solvrobackendtask/SolvroBackendTaskApplication.java
@@ -0,0 +1,12 @@
+package io.github.boavenn.solvrobackendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvroBackendTaskApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SolvroBackendTaskApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/boavenn/solvrobackendtask/SolvroBackendTaskApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.boavenn.solvrobackendtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvroBackendTaskApplicationTests
+{
+    @Test
+    void contextLoads() {
+    }
+}
